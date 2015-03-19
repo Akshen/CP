de763cb27445cb9f3b96819964aa289f34f51502
Akshen <doke.akshen@gmail.com>
2015-03-19 12:48:29 +0530

    First commit

--- problem.py
+++
@@ -1,8 +0,0 @@
-from math import factorial
-tc = input()
-while tc!=0:
-	tc -=1
-	n,k = map(int,raw_input().split())
-	n = factorial(n)
-	l = map(int,raw_input().split())
-	print n%max(l)
--- tree.py
+++
@@ -1,8 +0,0 @@
-from pylab import *
-	
-
-x=arange(0,5,0.05)
-y=2*sin(2*(x))
-
-plot(x,y)
-show()
